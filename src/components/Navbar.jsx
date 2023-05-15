b9281965c852a9ab5128934d6b087bac75fc968c
cesarlee38 <cesarlee38@hotmail.com>
2023-05-15 14:30:42 -0600

    tercerintento

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,13 +21,13 @@ function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav" style={centrarNavbar}>
                             <li className="nav-item">
-                                <Link className="nav-link active" to='./'>Inicio</Link>
+                                <Link className="nav-link active" to='/'>Inicio</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to='./Productos'>Productos</Link>
+                                <Link className="nav-link" to='/Productos'>Productos</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to='./About'>About</Link>
+                                <Link className="nav-link" to='/About'>About</Link>
                             </li>
                         </ul>
                     </div>
